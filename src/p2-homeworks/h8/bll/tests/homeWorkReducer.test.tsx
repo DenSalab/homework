bc070731a3c0192a2f6e3759430d59bf051f0997
Denis <denis@gmail>
2022-01-05 19:23:53 +0200

    tests for hw8

--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import {homeWorkReducer} from '../homeWorkReducer'
 
-let initialState: any[] // need to fix any
+type PeopleType = {
+    _id: number
+    name: string
+    age: number
+}
+type initialStateType = PeopleType[]
+
+let initialState: initialStateType
 
 beforeEach(() => {
     initialState = [
@@ -15,18 +22,51 @@ beforeEach(() => {
 })
 
 test('sort name up', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
+    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up', sort: 'name'})
 
-    console.log(newState)
-    // expect(...).toBe(...)
+    expect(newState[0].name).toBe('Александр');
+    expect(newState[1].name).toBe('Виктор');
+    expect(newState[2].name).toBe('Дмитрий');
+    expect(newState[3].name).toBe('Ирина');
+    expect(newState[4].name).toBe('Коля');
+    expect(newState[5].name).toBe('Кот');
 })
 test('sort name down', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
+    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down', sort: 'name'})
 
+    expect(newState[0].name).toBe('Кот');
+    expect(newState[1].name).toBe('Коля');
+    expect(newState[2].name).toBe('Ирина');
+    expect(newState[3].name).toBe('Дмитрий');
+    expect(newState[4].name).toBe('Виктор');
+    expect(newState[5].name).toBe('Александр');
+})
+test('sort age up', () => {
+    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up', sort: 'age'})
 
+    expect(newState[0].name).toBe('Кот');
+    expect(newState[1].name).toBe('Коля');
+    expect(newState[2].name).toBe('Дмитрий');
+    expect(newState[3].name).toBe('Виктор');
+    expect(newState[4].name).toBe('Ирина');
+    expect(newState[5].name).toBe('Александр');
+})
+test('sort age down', () => {
+    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down', sort: 'age'})
+
+    expect(newState[0].name).toBe('Александр');
+    expect(newState[1].name).toBe('Ирина');
+    expect(newState[2].name).toBe('Виктор');
+    expect(newState[3].name).toBe('Дмитрий');
+    expect(newState[4].name).toBe('Коля');
+    expect(newState[5].name).toBe('Кот');
 })
 test('check age 18', () => {
     const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
 
-
+    expect(newState.length).toBe(4);
+    expect(newState.sort()[0].name).toBe('Александр');
+    expect(newState.sort()[1].name).toBe('Виктор');
+    expect(newState.sort()[2].name).toBe('Дмитрий');
+    expect(newState.sort()[3].name).toBe('Ирина');
 })
