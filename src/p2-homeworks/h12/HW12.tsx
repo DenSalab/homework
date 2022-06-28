890c2b633f6cab20176040b3fc7797173438c054
Denis <denis140292@gmail.com>
2022-06-28 19:48:37 +0300

    lesson 12: "change theme" completed

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import s from "./HW12.module.css";
+import SuperSelect from "../h7/common/c5-SuperSelect/SuperSelect";
+import {useDispatch, useSelector} from "react-redux";
+import {changeThemeAC} from "../h10/bll/themeReducer";
+import {AppStoreType} from "../h10/bll/store";
 
-const themes = ['dark', 'red', 'some'];
+const themes = ['dark', 'red', 'green', 'blue'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
+    const theme = useSelector((state: AppStoreType): string => state.theme.theme);
 
-    // useDispatch, onChangeCallback
+    const dispatch = useDispatch();
+    const onChangeCallback = (theme: string) => {
+        dispatch(changeThemeAC({theme}))
+    }
 
     return (
         <div className={s[theme]}>
@@ -14,7 +21,12 @@ function HW12() {
             <span className={s[theme + '-text']}>
                 homeworks 12
             </span>
-
+            <br/>
+            <SuperSelect
+                options={themes}
+                value={theme}
+                onChangeOption={onChangeCallback}
+            />
             {/*should work (должно работать)*/}
             {/*SuperSelect or SuperRadio*/}
 
--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import {NavLink} from "react-router-dom";
 import {PATH} from './Routes';
 import s from './Header.module.css';
+import background from "../h12/HW12.module.css";
+import {useSelector} from "react-redux";
+import {AppStoreType} from "../h10/bll/store";
 
 function Header() {
+    const theme = useSelector((state: AppStoreType): string => state.theme.theme);
     return (
         <div className={s.wrapper}>
             <NavLink className={s.link} to={PATH.PRE_JUNIOR}>PreJunior</NavLink>
             <NavLink className={s.link} to={PATH.JUNIOR}>Junior</NavLink>
             <NavLink className={s.link} to={PATH.JUNIOR_PLUS}>Junior+</NavLink>
-            <div className={s.button}/>
+            <div className={`${s.button} ${background[theme]}`}/>
         </div>
     )
 }
